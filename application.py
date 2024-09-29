56f2d48ac6157ca0c40cec8a332036f91fda3cbc
Varun <varun19424@gmail.com>
2024-09-30 02:35:27 +0530

    Beanstalk

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 # @app.route('/')
